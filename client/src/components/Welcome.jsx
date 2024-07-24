f612dba5728bc2ea39a1560d1b73f96757c2178d
ManoelOscar35 <manoeloscar557@gmail.com>
2024-07-24 19:52:08 -0300

    criação do cartão ether e do formulário

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,14 +4,33 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from './'
 
-import React from 'react';
+import React, {useState} from 'react';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
+const Input = ({placeholder, name, type, value, handleChange}) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    value={value}
+    step="0.0001"
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+)
+
 const Welcome = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+  }
+
+
   const connectWallet = () => {
 
   }
+
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py:12 px-4">
@@ -41,7 +60,54 @@ const Welcome = () => {
             <div className={`rounded-tr-2xl ${commonStyles}`}>
               Ethereum
             </div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>
+              Web 3.0
+            </div>
+            <div className={commonStyles}>
+              Taxas Atrativas
+            </div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>
+              MetaMask
+            </div>
           </div>
+        </div>
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff"/>
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff"/>
+              </div>
+              <p className="text-white font-light text-sm">
+                Endereço
+              </p>
+              <p className="text-white font-semibold text-lg mt-1">
+                Ethereum
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+          <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}}/>
+          <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
+          <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}}/>
+          <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}}/>
+
+          <div className="h-[1px] w-full bg-gray-400 my-2"/>
+          {isLoading
+          ? <Loader/>
+          : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-white bg-[#2952e3] hover:bg-[#2546bd] rounded-full cursor-pointer"
+              >
+                Envie Agora
+              </button>
+            )  
+          }
         </div>
       </div>
     </div>
