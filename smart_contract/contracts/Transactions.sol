fce1596abc88cce283bd06cfb61843e52ae88aab
ManoelOscar35 <manoeloscar557@gmail.com>
2024-07-30 14:38:35 -0300

    implementando as funções e  estados das transações

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.24;
 
 contract Transactions {
+
     uint256 transactionsCount;
+    TransferStruct[] transactions;
 
     event Transfer(address from, address receiver, uint amount, string messsage, uint256 timestamp, string keyword);
 
@@ -15,9 +17,18 @@ contract Transactions {
         string keyword;
     }
 
-    TransferStruct[] transactions;
-
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
-        
+        transactionsCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransactions() public view returns(TransferStruct[] memory) {
+      return transactions;
+    }
+
+    function getTransactionCount() public view returns(uint256) {
+      return transactionsCount;
     }
 }
